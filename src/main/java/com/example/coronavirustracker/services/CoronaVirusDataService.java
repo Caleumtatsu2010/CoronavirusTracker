bfe3912ba0464d161c8b140a8440643a5c7446e2
Tatsu Caleum <shinyakogami2010@gmail.com>
2021-09-11 21:42:57 +0700

    Update CoronaVirusDataService.java

--- src/main/java/com/example/coronavirustracker/services/CoronaVirusDataService.java
+++ src/main/java/com/example/coronavirustracker/services/CoronaVirusDataService.java
@@ -1,5 +1,6 @@
 package com.example.coronavirustracker.services;
 
+import com.example.coronavirustracker.models.LocationStats;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -12,24 +13,39 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.*;
 
 @Service
 public class CoronaVirusDataService
 {
-    private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
-
+    private static String  VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+    List<LocationStats> allStats = new ArrayList<>();
     @PostConstruct
     @Scheduled(cron = "* * 1 * * *")//scheule to run every 1 hour
     public void fetchVirusData() throws IOException, InterruptedException {
+        List<LocationStats> newStats = new ArrayList<>();
+
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(VIRUS_DATA_URL)).build();
         HttpResponse <String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
 
         StringReader csvBodyReader = new StringReader(httpResponse.body());
         Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
+
         for (CSVRecord record : records) {
-            String state = record.get("Province/State");
-            System.out.println(state);
+            //initalize an object to store a record got from  csv file
+            LocationStats locationStats = new LocationStats();
+
+            //get data from Province/State collum and set to state in locationStats
+            locationStats.setState(record.get("Province/State"));
+            locationStats.setCountry(record.get("Country/Region"));
+            locationStats.getLatestTotalCases(Integer.parseInt(record.get(record.size()-1)));//get last row in record and set to totalcase
+            System.out.println(locationStats.toString());
+            newStats.add(locationStats);//add to list of record objects
+
+
         }
+        this.allStats = newStats;
     }
 }
