772f030c45b0b11cc8a168e3dde2148da0654624
Tatsu Caleum <shinyakogami2010@gmail.com>
2021-09-11 21:42:54 +0700

    Update LocationStats.java

--- src/main/java/com/example/coronavirustracker/models/LocationStats.java
+++ src/main/java/com/example/coronavirustracker/models/LocationStats.java
@@ -1,5 +1,40 @@
 package com.example.coronavirustracker.models;
 
 public class LocationStats {
-    
+    private String state;
+    private String country;
+    private String latestTotalCases;
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getLatestTotalCases(int i) {
+        return latestTotalCases;
+    }
+
+    public void setLatestTotalCases(String latestTotalCases) {
+        this.latestTotalCases = latestTotalCases;
+    }
+
+    @Override
+    public String toString() {
+        return "LocationStats{" +
+                "state='" + state + '\'' +
+                ", country='" + country + '\'' +
+                ", latestTotalCases='" + latestTotalCases + '\'' +
+                '}';
+    }
 }
