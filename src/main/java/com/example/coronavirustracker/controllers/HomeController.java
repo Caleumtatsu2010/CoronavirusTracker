501d366318da343ee0c9f99ed4dd52eeb2041b96
Tatsu Caleum <shinyakogami2010@gmail.com>
2021-09-11 23:07:51 +0700

    Update HomeController.java

--- src/main/java/com/example/coronavirustracker/controllers/HomeController.java
+++ src/main/java/com/example/coronavirustracker/controllers/HomeController.java
@@ -1,14 +1,32 @@
 package com.example.coronavirustracker.controllers;
 
+import com.example.coronavirustracker.CoronavirusTrackerApplication;
+import com.example.coronavirustracker.models.LocationStats;
+import com.example.coronavirustracker.services.CoronaVirusDataService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    CoronaVirusDataService coronaVirusDataService = null;
+
     @GetMapping("/")
-    public String home()
+    public String home(Model model)
     {
+        List<LocationStats> allStats = coronaVirusDataService.getAllStats();//get all object record
+
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+        //merge all param to model
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalReportedCases", totalReportedCases);
+        model.addAttribute("totalNewCases", totalNewCases);
         return "home";
     }
 }
